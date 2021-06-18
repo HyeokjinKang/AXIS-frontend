5492d0dd7b9019dd43321a731507dd3e9820ad03
HyeokjinKang <coupy@croissant.tech>
2021-06-19 05:13:02 +0900

    add: i18n 적용

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const config = require("./config/config.json");
 
 //libraries
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const i18n = require("./i18n");
 
 //express environment
 const port = 1026;
 const app = express();
+app.locals.pretty = true;
 
 //express settings
 app.set("view engine", "ejs");
@@ -15,6 +18,28 @@ app.set("views", __dirname + "\\views");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "\\public"));
+app.use(cookieParser());
+app.use(i18n);
+
+app.get("/en", (req, res) => {
+  res.cookie("lang", "en");
+  res.redirect("/main");
+});
+
+app.get("/ko", (req, res) => {
+  res.cookie("lang", "ko");
+  res.redirect("/main");
+});
+
+app.get("/cn", (req, res) => {
+  res.cookie("lang", "cn");
+  res.redirect("/main");
+});
+
+app.get("/jp", (req, res) => {
+  res.cookie("lang", "jp");
+  res.redirect("/main");
+});
 
 app.get("/", (req, res) => {
   res.render("index", { api: config.project.api, front: config.project.front });
